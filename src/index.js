a79b47c043203845588770a7bff8f960058519cc
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2019-01-19 00:37:21 +0900

    implement evetn new component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer,applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <EventsIndex />
-    </Provider>,
-    document.getElementById('root')
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={EventsNew} />
+                <Route exact path="/" component={EventsIndex} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
