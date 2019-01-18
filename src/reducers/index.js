55607fdc01572614d56b790af0b44bead63792fe
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2019-01-19 08:52:21 +0900

    implement event new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
+import {reducer as form} from 'redux-form'
 import { combineReducers } from 'redux'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
