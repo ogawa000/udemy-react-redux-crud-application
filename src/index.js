cc81d8f4e610323c9a99b6a1256fca80884d4d97
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2019-01-21 03:18:21 +0900

    implement submit button

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
-
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer,applyMiddleware(thunk))
 
@@ -24,8 +22,3 @@ ReactDOM.render(
     </Provider>
     ,document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
